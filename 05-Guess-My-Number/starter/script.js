25d4fd6a4c62c206de4cd7764b4ed127ba2333ec
Jayaprakashs-AdapptLimited <jayaprakash.s@adappt.ai>
2023-11-10 11:36:22 +0530

    first commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct';
+
+console.log(document.querySelector('.number').textContent = 10);
+document.querySelector('.score').textContent = 20;
+document.querySelector('.guess').value = 13;
+
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener(
+    'click', function () {
+        console.log(document.querySelector('.guess').value);
+    }
+)
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+    // displayMessage('No number!');
+  }
